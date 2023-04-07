485b787c5ba588f1087601df3d0c3fe72d700941
Lucas Segurado <lsegurado1996@gmail.com>
2023-04-07 20:48:11 +0200

    Updated template

--- michi.config.ts
+++ michi.config.ts
@@ -1,35 +1,28 @@
 import { ServerConfigFactory } from '@michijs/dev-server';
 
+const icons = [24, 48, 72, 96, 128, 256, 512].map((x) => ({
+  src: `/assets/generated/icon-${x}.webp`,
+  sizes: `${x}x${x}`,
+  type: 'image/webp',
+}));
+
 export const config: ServerConfigFactory = () => ({
   public: {
     manifest: {
       options: {
-        name: "MichiJS",
-        short_name: "MichiJS",
-        description: "Template for MichiJS library",
-        theme_color: "#FEDB35",
-        background_color: "#FEDB35",
-        display: "standalone",
-        orientation: "portrait",
-        start_url: "/",
-        scope: ".",
-        icons: [
-          {
-            src: "/icon.svg",
-            sizes: "24x24 48x48 72x72 96x96 128x128 256x256",
-            type: "image/svg+xml",
-            purpose: "any maskable"
-          },
-          {
-            src: "/splash-screen.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
-      }
-    }
-  }
+        name: 'MichiJS',
+        short_name: 'MichiJS',
+        description: 'Template for MichiJS library',
+        theme_color: '#FEDB35',
+        background_color: '#FEDB35',
+        display: 'standalone',
+        orientation: 'portrait',
+        start_url: '/',
+        scope: '.',
+        icons,
+      },
+    },
+  },
 });
 
 export default config;
--- public/service-worker.ts
+++ public/service-worker.ts
@@ -1,31 +1,47 @@
 /// <reference no-default-lib="true"/>
 /// <reference lib="esNext" />
 /// <reference lib="WebWorker" />
-import type { ProcessSWType } from '@michijs/dev-server'
+import type { ProcessSWType } from '@michijs/dev-server';
 
 const sw = self as ServiceWorkerGlobalScope & typeof globalThis;
 
-declare var process: ProcessSWType;
+declare let process: ProcessSWType;
 
 const buildFiles = process.env.BUILD_FILES;
 const cacheName = process.env.CACHE_NAME;
 
-sw.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      return cache.addAll(buildFiles).then(() => { sw.skipWaiting(); });
-    })
+const expectedCaches = [cacheName];
+
+const storeBuildFilesIntoCache = async () => {
+  const cache = await caches.open(cacheName);
+  return await cache.addAll(buildFiles);
+};
+
+const controlPageAndClean = async () => {
+  sw.clients.claim();
+  const cacheNames = await caches.keys();
+  return await Promise.all(
+    cacheNames.map((x) =>
+      expectedCaches.includes(x) ? undefined : caches.delete(x),
+    ),
   );
+};
+
+const getFromCacheOrFetch = async (e: FetchEvent) => {
+  const response = await caches.match(e.request);
+  return response || fetch(e.request);
+};
+
+// Cache, falling back to network strategy
+sw.addEventListener('install', (e) => {
+  sw.skipWaiting();
+  e.waitUntil(storeBuildFilesIntoCache());
 });
 
-sw.addEventListener("activate", (e) => {
-  e.waitUntil(sw.clients.claim());
+sw.addEventListener('activate', (e) => {
+  e.waitUntil(controlPageAndClean());
 });
 
-sw.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.open(cacheName)
-      .then((cache) => { return cache.match(e.request, { ignoreSearch: true }); })
-      .then((response) => { return response || fetch(e.request); })
-  );
+sw.addEventListener('fetch', (e) => {
+  e.respondWith(getFromCacheOrFetch(e));
 });
